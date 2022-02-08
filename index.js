d86b1993201637fb67c3c61d604fb6c3bab64041
Nicholas A Christensen <nickachristensen@gmail.com>
2022-02-08 17:47:30 -0500

    commit tree 14abe40d6a4a21bb635cd24288c7b18591ce08ab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution here
+//first set
+function findMatching(drivers, string) {
+   return drivers.filter(function(driver) {
+      return driver.toLowerCase() === string.toLowerCase()
+   })
+}
+//second set
+// function findMatching(drivers, string) {
+//     return drivers.filter( driver =>
+//       driver.toLowerCase() === string.toLowerCase()
+//     )
+// }
+
+// const findMatching = function(drivers, string) {
+//     return drivers.filter(function(driver) {
+//       return driver.toLowerCase() === string.toLowerCase()
+//     })
+// }
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver[0] === string[0];
+    })
+}
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter( possibleMatch =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     )
+//   }
+
+function matchName(drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.name === string;
+    })
+}
+
+// function matchName(source, soughtName) {
+//     return source.filter( record => record.name === soughtName)
+//   }
